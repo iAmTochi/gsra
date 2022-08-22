02cbb27d7be00f1c76e18f9be32c135df1531a88
Tochukwu Ugwu <ugwukelvintochukwu@gmail.com>
2022-08-22 13:53:55 +0100

    Create about-us.blade.php

---
+++ resources/views/about-us.blade.php
@@ -0,0 +1,143 @@
+@extends('layouts.home')
+
+@section('content')
+    <!-- ======================= About Us Detail ======================== -->
+    <section class="middle">
+        <div class="container">
+            <div class="row align-items-center justify-content-between">
+
+                <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12">
+                    <div class="abt_caption">
+                        <h2 class="ft-medium mb-4">We Have Everything You Need ?</h2>
+                        <p class="mb-4">Global Skills Recruitment Agency, GSRA, is a recruiting, training
+                            and staffing agency. Our number one goal is to significantly reduce the number
+                            of unemployed graduates in Nigeria. To achieve our primary objective,
+                            we dedicate a lot of time to training and re-training candidates to be
+                            employable and fitted for the right job. We hire for fit by connecting qualified
+                            employees to their respective employers.
+                        </p>
+                        <p class="mb-4">At GSRA, we are focused on making candidates highly productive at the
+                            workplace. To do that, we undertake a lot of training on behalf of our clients and
+                            the candidates themselves to ensure that they are equipped with the requisite
+                            knowledge and skills to perform their responsibilities effectively.
+                            We are a one-stop shop for the basic recruitment needs of both employers and
+                            job seekers and have inculcated SKILLS ACQUISITION as a fundamental aspect of
+                            GSRA to help prepare our candidates to be successful in their next job.</p>
+                        <div class="form-group mt-4">
+{{--                            <a href="#" class="btn theme-bg text-white">See More Info</a>--}}
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12">
+                    <div class="abt_caption">
+                        <img src="assets/img/about-1.png" class="img-fluid rounded" alt="" />
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+    <!-- ======================= About Us End ======================== -->
+
+
+        <!-- ======================= Blog Start ============================ -->
+        <section class="space min gray">
+            <div class="container">
+
+{{--                <div class="row justify-content-center">--}}
+{{--                    <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12">--}}
+{{--                        <div class="sec_title position-relative text-center mb-4">--}}
+{{--                            <h6 class="text-muted mb-0">Latest News</h6>--}}
+{{--                            <h2 class="ft-bold">Pickup New Updates</h2>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+
+                <div class="row justify-content-center">
+
+                    <!-- Single Item -->
+                    <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12">
+                        <div class="blg_grid_box">
+
+                            <div class="blg_grid_caption">
+                                <div class="blg_title"><h4>OUR VISION </h4></div>
+
+                                <div class="blg_desc">
+                                    <p>To significantly reduce the number of unemployed
+                                        persons in Nigeria; and retrain them
+                                        to be purposefully fit for the quality development of self and society.
+                                    </p>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                    <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12">
+                        <div class="blg_grid_box">
+
+                            <div class="blg_grid_caption">
+
+                                <div class="blg_title"><h4>OUR MISSION </h4></div>
+                                <div class="blg_desc">
+                                    <p>Our goal is multidimensional. We fit square pegs in square holes
+                                        and we recruit, train, retrain and equip individuals with the
+                                        appropriate skill-sets to be productive in the workplace.
+                                        We weave through the thick of applicants until we get out the right fit.
+                                    </p>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                    <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12">
+                        <div class="blg_grid_box">
+
+                            <div class="blg_grid_caption">
+
+                                <div class="blg_title"><h4>OUR PHILOSOPHY</h4></div>
+                                <div class="blg_desc">
+                                    <p>Having the right crop of staff is important to any business.
+                                        We connect suitably qualified workforce with the right Employers.
+                                    </p>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+        </section>
+        <!-- ======================= Blog Start ============================ -->
+
+
+    <!-- ======================= About Us Detail ======================== -->
+    <section class="middle">
+        <div class="container">
+            <div class="row align-items-center justify-content-between">
+
+                <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12">
+                    <div class="abt_caption">
+                        <img src="assets/img/about-2.png" class="img-fluid rounded" alt="" />
+                    </div>
+                </div>
+
+                <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12">
+                    <div class="abt_caption">
+                        <h2 class="ft-medium mb-4">Justin Lisiakir</h2>
+                        <p class="mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                        <p class="mb-4">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus.</p>
+                        <div class="form-group mt-4">
+                            <a href="#" class="btn theme-bg text-white">See More Info</a>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+    <!-- ======================= About Us End ======================== -->
+
+@endsection
