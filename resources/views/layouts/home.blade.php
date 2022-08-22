0c62ecbbb7b7f11f5308cc4aec614fc85d669d9a
Tochukwu Ugwu <ugwukelvintochukwu@gmail.com>
2022-08-22 13:54:17 +0100

    Update home.blade.php

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -47,7 +47,7 @@
                 <div class="nav-menus-wrapper" style="transition-property: none;">
                     <ul class="nav-menu">
 
-                        <li><a href="#">Home</a>
+{{--                        <li><a href="{{ route('home') }}">Home</a>--}}
                             {{-- <ul class="nav-dropdown nav-submenu">
                                 <li><a href="index.html">Home 1</a></li>
                                 <li><a href="home-2.html">Home 2</a></li>
@@ -58,7 +58,8 @@
                                 <li><a href="home-7.html">Home 7</a></li>
                                 <li><a href="home-8.html">Home 8</a></li>
                             </ul> --}}
-                        </li>
+{{--                        </li>--}}
+
 
                         <li><a href="javascript:void(0);">Find Job</a>
                             <ul class="nav-dropdown nav-submenu">
@@ -70,23 +71,16 @@
 
                         </li>
 
-                        <li><a href="javascript:void(0);">Job Seekers</a>
-                            <ul class="nav-dropdown nav-submenu">
-                                <li><a href="browse-jobs.html">Browse Jobs</a></li>
-                                <li><a href="browse-resumes.html">Browse Resumes</a></li>
-                                <li><a href="browse-category.html">Browse Categories</a></li>
-                                <
-                            </ul>
-                        </li>
-
                         <li><a href="javascript:void(0);">Employers</a>
-                            <ul class="nav-dropdown nav-submenu">
-                                <li><a href="browse-employers.html">Browse Employers V1</a></li>
-                                <li><a href="browse-employers-list.html">Browse Employers V2</a></li>
-                                <li><a href="employer-detail.html">Employer Detail</a></li>
-                                <li><a href="employer-dashboard.html">Employer Dashboard</a></li>
-                            </ul>
+{{--                            <ul class="nav-dropdown nav-submenu">--}}
+{{--                                <li><a href="browse-employers.html">Browse Employers V1</a></li>--}}
+{{--                                <li><a href="browse-employers-list.html">Browse Employers V2</a></li>--}}
+{{--                                <li><a href="employer-detail.html">Employer Detail</a></li>--}}
+{{--                                <li><a href="employer-dashboard.html">Employer Dashboard</a></li>--}}
+{{--                            </ul>--}}
                         </li>
+                        <li><a href="{{ route('about') }}">About</a></li>
+                        <li><a href="{{ route('contact') }}">Contact</a></li>
 
 
 
@@ -119,12 +113,65 @@
     <!-- ============================================================== -->
     <!-- Top header  -->
     <!-- ============================================================== -->
+@if($pageTitle??Null)
+    <!-- ======================= Top Breadcrubms ======================== -->
+    <div class="gray py-3">
+        <div class="container">
+            <div class="row">
+                <div class="colxl-12 col-lg-12 col-md-12">
+                    <nav aria-label="breadcrumb">
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="#">Home</a></li>
+                            <li class="breadcrumb-item active" aria-current="page">{{ $pageTitle }}</li>
+                        </ol>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- ======================= Top Breadcrubms ======================== -->
+@endif
 
 
 
         @yield('content')
 
+<!-- ======================= Newsletter Start ============================ -->
+    <section class="space bg-cover" style="background:#03343b url(assets/img/landing-bg.png) no-repeat;">
+        <div class="container py-5">
 
+            <div class="row justify-content-center">
+                <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12">
+                    <div class="sec_title position-relative text-center mb-5">
+                        <h6 class="text-light mb-0">Subscribr Now</h6>
+                        <h2 class="ft-bold text-light">Get All New Job Notification</h2>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row align-items-center justify-content-center">
+                <div class="col-xl-7 col-lg-10 col-md-12 col-sm-12 col-12">
+                    <form class="bg-white rounded p-1">
+                        <div class="row no-gutters">
+                            <div class="col-xl-9 col-lg-9 col-md-8 col-sm-8 col-8">
+                                <div class="form-group mb-0 position-relative">
+                                    <input type="text" class="form-control lg left-ico" placeholder="Enter Your Email Address">
+                                    <i class="bnc-ico lni lni-envelope"></i>
+                                </div>
+                            </div>
+                            <div class="col-xl-3 col-lg-3 col-md-4 col-sm-4 col-4">
+                                <div class="form-group mb-0 position-relative">
+                                    <button class="btn full-width custom-height-lg theme-bg text-light fs-md" type="button">Subscribe</button>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+
+        </div>
+    </section>
+    <!-- ======================= Newsletter Start ============================ -->
     <!-- ============================ Footer Start ================================== -->
     <footer class="light-footer skin-light-footer style-2">
         <div class="footer-middle">
@@ -136,10 +183,19 @@
                             <img src="assets/img/logo.png" class="img-footer small mb-2" alt="" />
 
                             <div class="address mt-2">
-                                3298 Grant Street Longview, New Texox<br>United Kingdom 75601
+
+                                Block 26, Plot 12, Admiralty Way, <br>Lekki Phase 1, Lagos, Nigeria
+
                             </div>
                             <div class="address mt-3">
-                                1-202-555-0106<br>support@workplex.com
+                                07039292227<br>
+                                07049214930<br>
+                                08116047012<br>
+                                <br>
+
+                                support@gsrajobs.com<br>
+                                gsrarecruit@gmail.com<br>
+                                jobsglobalrecruitment21@gmail.com
                             </div>
                             <div class="address mt-2">
                                 <ul class="list-inline">
@@ -158,10 +214,10 @@
                             <h4 class="widget_title">For Employers</h4>
                             <ul class="footer-menu">
                                 <li><a href="#">Explore Candidates</a></li>
-                                <li><a href="#">Job Pricing</a></li>
+{{--                                <li><a href="#">Job Pricing</a></li>--}}
                                 <li><a href="#">Submit Job</a></li>
                                 <li><a href="#">Shortlisted</a></li>
-                                <li><a href="#">Dashboard</a></li>
+{{--                                <li><a href="#">Dashboard</a></li>--}}
                             </ul>
                         </div>
                     </div>
@@ -172,8 +228,8 @@
                             <ul class="footer-menu">
                                 <li><a href="#">Explore All Jobs</a></li>
                                 <li><a href="#">Browse Categories</a></li>
-                                <li><a href="#">Saved Jobs</a></li>
-                                <li><a href="#">Dashboard</a></li>
+{{--                                <li><a href="#">Saved Jobs</a></li>--}}
+{{--                                <li><a href="#">Dashboard</a></li>--}}
                             </ul>
                         </div>
                     </div>
@@ -182,11 +238,11 @@
                         <div class="footer_widget">
                             <h4 class="widget_title">About Company</h4>
                             <ul class="footer-menu">
-                                <li><a href="#">Who We'r?</a></li>
+                                <li><a href="#">Who We're?</a></li>
                                 <li><a href="#">Our Mission</a></li>
                                 <li><a href="#">Our team</a></li>
-                                <li><a href="#">Packages</a></li>
-                                <li><a href="#">Dashboard</a></li>
+{{--                                <li><a href="#">Packages</a></li>--}}
+{{--                                <li><a href="#">Dashboard</a></li>--}}
                             </ul>
                         </div>
                     </div>
@@ -195,10 +251,10 @@
                         <div class="footer_widget">
                             <h4 class="widget_title">Helpful Topics</h4>
                             <ul class="footer-menu">
-                                <li><a href="#">Site Map</a></li>
-                                <li><a href="#">Security</a></li>
+{{--                                <li><a href="#">Site Map</a></li>--}}
+{{--                                <li><a href="#">Security</a></li>--}}
                                 <li><a href="#">Contact</a></li>
-                                <li><a href="#">FAQ's Page</a></li>
+                                <li><a href="#">FAQ's</a></li>
                                 <li><a href="#">Privacy</a></li>
                             </ul>
                         </div>
@@ -212,7 +268,7 @@
             <div class="container">
                 <div class="row align-items-center">
                     <div class="col-lg-12 col-md-12 text-center">
-                        <p class="mb-0">© 2022 Workplex. Designd By <a href="https://themezhub.com/">ThemezHub</a>.</p>
+                        <p class="mb-0">© {{ date('Y') }} Global Skill Recruitment Agency Limited. <!-- Designd By <a href="https://themezhub.com/">ThemezHub</a>.--> </p>
                     </div>
                 </div>
             </div>
