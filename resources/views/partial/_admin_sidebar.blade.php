36984c6d93ccb3fd9af0c71f6d67161632cb68e0
Tochukwu Ugwu <ugwukelvintochukwu@gmail.com>
2022-09-16 14:50:21 +0100

    Update _admin_sidebar.blade.php

--- resources/views/partial/_admin_sidebar.blade.php
+++ resources/views/partial/_admin_sidebar.blade.php
@@ -4,21 +4,51 @@
             <ul data-submenu-title="Main Navigation">
                 <li class="active"><a href="{{ route('login') }}"><i class="lni lni-dashboard mr-2"></i>Dashboard</a></li>
                 <li class="accordion">
-                    <a href="" data-toggle="collapse" data-target="#manage-job" aria-expanded="true" aria-controls="manage-job"><i class="lni lni-add-files mr-2"></i>Manage Jobs</a>
-                    @if(auth()->user()->role == "employer")
-                    <ul id="manage-job" class="collapse">
-                        <li><a href="{{ route('jobs.index') }}"><i class="lni lni-files mr-2"></i>View All Jobs<span class="count-tag bg-warning">4</span></a></li>
-                        <li><a href="{{ route('jobs.create') }}"><i class="lni lni-add-files mr-2"></i>Post New Job </a></li>
-                    </ul>
-                    @endif
-                    @if(auth()->user()->role == "admin")
-                    <ul id="manage-job" class="collapse">
-                        <li><a href="{{ route('manage-jobs.index') }}"><i class="lni lni-files mr-2"></i>View All Jobs<span class="count-tag bg-warning">4</span></a></li>
+                    @if(auth()->user()->isEmployer())
 
-                    </ul>
-                    @endif
+                        <a  data-toggle="collapse" data-target="#manage-job" aria-expanded="true" aria-controls="manage-job"><i class="lni lni-briefcase mr-2"></i>Manage Jobs</a>
+                        <ul id="manage-job" class="collapse">
+                            <li><a href="{{ route('jobs.index') }}"><i class="lni lni-files mr-2"></i>View All Jobs<span class="count-tag bg-warning">4</span></a></li>
+                            <li><a href="{{ route('jobs.create') }}"><i class="lni lni-add-files mr-2"></i>Post New Job </a></li>
+                        </ul>
 
+                    @elseif(auth()->user()->isAdmin())
+
+                        <a href="" data-toggle="collapse" data-target="#manage-job" aria-expanded="true" aria-controls="manage-job"><i class="lni lni-briefcase mr-2"></i>Manage Jobs</a>
+                        <ul id="manage-job" class="collapse">
+                            <li><a href="{{ route('manage-jobs.index') }}"><i class="lni lni-files mr-2"></i>View All Jobs<span class="count-tag bg-warning">4</span></a></li>
+
+                        </ul>
+
+                    @else
                 </li>
+
+                    <li class="">
+                        <a href="{{ route('applicant.add.resume') }}"><i class="lni lni-add-files mr-2"></i>Create Resume</a>
+                    </li>
+                    <li>
+                        <a href="{{ route('applicant.jobs') }}"><i class="lni lni-files mr-2"></i>Applied Jobs<span class="count-tag bg-warning">4</span></a>
+                    </li>
+
+
+
+
+
+
+                    @endif
+
+                @if(auth()->user()->isEmployer())
+{{--                    <li class="accordion">--}}
+{{--                        <a  data-toggle="collapse" data-target="#manage-applicant" aria-expanded="true" aria-controls="manage-applicant"><i class="lni lni-briefcase mr-2"></i>Manage Applicants</a>--}}
+{{--                        <ul id="manage-applicant" class="collapse">--}}
+{{--                            <li><a href="dashboard-shortlisted-resume.html"><i class="lni lni-files mr-2"></i>View All Applicants<span class="count-tag bg-warning">4</span></a></li>--}}
+{{--                        </ul>--}}
+
+{{--                    </li>--}}
+                    <li class="active"><a href="{{ route('applicants') }}"><i class="lni lni-briefcase mr-2"></i>Manage Applicants</a></li>
+                @endif
+
+                @if(auth()->user()->isAdmin())
                 <li class="accordion">
                     <a href="" data-toggle="collapse" data-target="#manage-user" aria-expanded="true" aria-controls="manage-user"><i class="lni lni-add-files mr-2"></i>Manage Users</a>
                     <ul id="manage-user" class="collapse">
@@ -28,14 +58,9 @@
                     </ul>
 
                 </li>
-                <li class="accordion">
-                    <a href="" data-toggle="collapse" data-target="#manage-applicant" aria-expanded="true" aria-controls="manage-applicant"><i class="lni lni-briefcase mr-2"></i>Manage Applicants</a>
-                    <ul id="manage-applicant" class="collapse">
-                        <li><a href="dashboard-shortlisted-resume.html"><i class="lni lni-files mr-2"></i>View All Applicants<span class="count-tag bg-warning">4</span></a></li>
-                    </ul>
+                @endif
 
-                </li>
-                <li><a href="dashboard-messages.html"><i class="lni lni-envelope mr-2"></i>Messages<span class="count-tag">4</span></a></li>
+                <li><a href="{{ route('messages') }}"><i class="lni lni-envelope mr-2"></i>Messages<span class="count-tag">4</span></a></li>
 
 
 
@@ -43,9 +68,9 @@
 
             </ul>
             <ul data-submenu-title="My Accounts">
-                <li><a href="dashboard-my-profile.html"><i class="lni lni-user mr-2"></i>My Profile </a></li>
-                <li><a href="dashboard-change-password.html"><i class="lni lni-lock-alt mr-2"></i>Change Password</a></li>
-                <li><a href="javascript:void(0);"><i class="lni lni-trash-can mr-2"></i>Delete Account</a></li>
+                <li><a href="{{ route('profile') }}"><i class="lni lni-user mr-2"></i>My Profile </a></li>
+                <li><a href="{{ route('change.password') }}"><i class="lni lni-lock-alt mr-2"></i>Change Password</a></li>
+
                 <li>
                     <a href="{{ route('logout') }}"
                        onclick="event.preventDefault();
