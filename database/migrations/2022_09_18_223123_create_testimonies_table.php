e9805dbeea5bd9e3289ad4e0fc1443b60d183bc2
Tochukwu Ugwu <ugwukelvintochukwu@gmail.com>
2022-09-21 11:11:47 +0100

    Update 2022_09_18_223123_create_testimonies_table.php

--- database/migrations/2022_09_18_223123_create_testimonies_table.php
+++ database/migrations/2022_09_18_223123_create_testimonies_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('testimonies', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('occupation')->default('Client');
+            $table->string('occupation')->nullable();
             $table->string('passport');
             $table->longText('description');
             $table->timestamps();
