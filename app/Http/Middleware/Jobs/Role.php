746112bb3f7b11d874f88cf7902ea44a81ac07ad
Tochukwu Ugwu <ugwukelvintochukwu@gmail.com>
2022-09-23 13:18:58 +0100

    Update Role.php

--- app/Http/Middleware/Jobs/Role.php
+++ app/Http/Middleware/Jobs/Role.php
@@ -29,7 +29,7 @@ class Role
 
         if (auth()->user()->isApplicant()) {
 
-            return redirect()->route('applicant.dashboard');
+            return redirect()->route('profile');
         }
 
 
