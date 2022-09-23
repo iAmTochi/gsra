c3e0f9428f562a528f979ee5f33c678f19e35201
Tochukwu Ugwu <ugwukelvintochukwu@gmail.com>
2022-09-23 14:29:45 +0100

    created JobApplication Model, controller, seed and migration

---
+++ database/migrations/2022_09_23_130113_create_job_applications_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('job_applications', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('job_id')->constrained();
+            $table->foreignId('applicant_id')->constrained();
+            $table->foreignId('resume_id')->nullable()->constrained();
+            $table->text('doc');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('job_applications');
+    }
+};
