b3ec09ccef91699d0fb836403f8f5a63790ac5da
Tochukwu Ugwu <ugwukelvintochukwu@gmail.com>
2022-10-28 16:56:49 +0100

    Add Article Model Relationship

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -4,8 +4,73 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Storage;
 
 class Article extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $guarded = [];
+
+
+    /**
+     * Delete post image from storage
+     *
+     * @return void
+     */
+    public function deleteImage()
+    {
+
+        Storage::delete($this->image);
+
+    }
+
+
+    public function articleCategory()
+    {
+
+        return $this->belongsTo(ArticleCategory::class);
+    }
+
+    public function articleTags(): BelongsToMany
+    {
+
+        return $this->belongsToMany(ArticleTag::class);
+    }
+
+
+    /**
+     * check if post has a tag
+     *
+     *
+     * @param $tagId
+     * @return bool
+     */
+    public function hasTag($tagId)
+    {
+
+        return in_array($tagId, $this->articleTags->pluck('id')->toArray());
+    }
+
+    public function user()
+    {
+
+        return $this->belongsTo(User::class);
+
+    }
+
+
+
+
+    public function scopeSearched($query)
+    {
+
+        $search = request()->query('search');
+
+
+
+        return $query->where('title', 'LIKE', "%{$search}%");
+    }
 }
