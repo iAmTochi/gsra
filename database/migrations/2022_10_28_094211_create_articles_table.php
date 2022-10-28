c6429e1c4d3b773066065593dd1451c6aff915c9
Tochukwu Ugwu <ugwukelvintochukwu@gmail.com>
2022-10-28 16:56:24 +0100

    Update 2022_10_28_094211_create_articles_table.php

--- database/migrations/2022_10_28_094211_create_articles_table.php
+++ database/migrations/2022_10_28_094211_create_articles_table.php
@@ -13,12 +13,13 @@ return new class extends Migration
      */
     public function up()
     {
+        Schema::dropIfExists('articles');
         Schema::create('articles', function (Blueprint $table) {
             $table->id();
 
             $table->foreignId('user_id')->constrained();
 
-            $table->foreignId('category_id')->constrained();
+            $table->foreignId('article_category_id')->constrained();
 
             $table->string('title');
 
