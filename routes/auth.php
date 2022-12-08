cacb4ce30e4619238020b6048dd087ea26e1bc3a
Tochukwu Ugwu <ugwukelvintochukwu@gmail.com>
2022-12-08 11:59:49 +0100

    Separate Jobseeker and Employer register

--- routes/auth.php
+++ routes/auth.php
@@ -11,8 +11,10 @@ use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
-    Route::get('register', [RegisteredUserController::class, 'create'])
+    Route::get('register', [RegisteredUserController::class, 'index'])
                 ->name('register');
+    Route::get('job-seeker/register', [RegisteredUserController::class, 'create'])
+        ->name('register.job-seeker');
     Route::get('employer/register',[RegisteredUserController::class, 'registerEmployer'])
         ->name('register.employer');
 
