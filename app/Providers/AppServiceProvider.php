a713c7099342c01fc0ab45939dff9360a5a95036
Tochukwu Ugwu <ugwukelvintochukwu@gmail.com>
2023-01-06 13:14:35 +0100

    Register costume directives

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +25,30 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $this->registerMacros();
+    }
+
+    public function registerMacros()
+    {
+        $this->registerCustomBladeDirectives();
+    }
+
+    public function registerCustomBladeDirectives()
+    {
+        Blade::if('admin', function (User $user = null) {
+            return ($user ?? user())->isAdmin();
+        });
+
+        Blade::if('employer', function (User $user = null) {
+            return ($user ?? user())->isEmployer();
+        });
+
+        Blade::if('applicant', function (User $user = null) {
+            return ($user ?? user())->isApplicant();
+        });
+
+        Blade::directive('money', function ($expression) {
+            return "<?php echo number_format($expression, 2) ?>";
+        });
     }
 }
