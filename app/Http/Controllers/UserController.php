8c25619eebb3ba70a608496480c2a41c7ff9991e
Tochukwu Ugwu <ugwukelvintochukwu@gmail.com>
2023-01-06 13:15:52 +0100

    Remove unnecessary function

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -15,7 +15,7 @@ class UserController extends Controller
     public function index()
     {
         $users = User::all();
-        return view('admin.user.users-list',compact('users'))->withCount(0);
+        return view('admin.user.users-list',compact('users'));
     }
 
     /**
