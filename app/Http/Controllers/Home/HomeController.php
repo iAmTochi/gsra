3781f43803d3a495421ad8ee9f65684f68007d0d
Tochukwu Ugwu <ugwukelvintochukwu@gmail.com>
2023-01-12 16:54:53 +0100

    Update HomeController.php

--- app/Http/Controllers/Home/HomeController.php
+++ app/Http/Controllers/Home/HomeController.php
@@ -6,27 +6,43 @@ use App\Http\Controllers\Controller;
 use App\Models\Job;
 use App\Models\Testimony;
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class HomeController extends Controller
 {
-    public function home(){
-
+    /**
+     * @return View
+     */
+    public function home():View
+    {
         $data = [
             'testimonies' => Testimony::all(),
             'jobs' => Job::limit(4)->get(),
         ];
-
         return view('home', $data);
-
     }
 
-    public function about(){
-
+    /**
+     * @return View
+     */
+    public function about() :View
+    {
         return view('about-us');
     }
 
-    public function contact(){
-
+    /**
+     * @return View
+     */
+    public function contact() :View
+    {
         return view('contact-us');
+    }
+
+    /**
+     * @return View
+     */
+    public function gallery() :View
+    {
+        return view('gallery');
     }
 }
