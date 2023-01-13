22139a56c81bbd3ba89eeebbbd2b187ce7944597
Tochukwu Ugwu <ugwukelvintochukwu@gmail.com>
2023-01-13 10:42:32 +0100

    Fetch gallery for the homepage

--- resources/views/admin/gallery/index.blade.php
+++ resources/views/admin/gallery/index.blade.php
@@ -18,7 +18,6 @@
                 </div>
             </div>
         </div>
-
         <div class="dashboard-widg-bar d-block">
             <div class="row">
                 <div class="col-xl-12 col-lg-12 col-md-12">
@@ -28,7 +27,6 @@
                                 <h4 class="mb-0 ft-medium fs-md"><i class="fa fa-file mr-1 theme-cl fs-sm"></i>{{ isset($gallery) ?'Edit ':'Add '}} Image</h4>
                             </div>
                         </div>
-
                         <div class="_dashboard_content_body py-3 px-3">
                             <form class="row" action="{{  isset($gallery) ? route('galleries.update', $gallery->id) : route('galleries.store') }}" method="post" enctype="multipart/form-data">
                                 @csrf
@@ -43,90 +41,80 @@
                                                 </span>
                                             </div>
                                         </div>
-
                                         <div class=" col-md-6">
                                             <div class="form-group">
-                                                <label class="text-dark ft-medium">{{ isset($gallery) ? 'Change Passport?':'Passport' }}</label>
+                                                <label class="text-dark ft-medium">{{ isset($gallery) ? 'Change Image?':'Image' }}</label>
                                                 <input type="file" value="{{ old('photo') }}" name="photo" class="form-control rounded @error('photo') is-invalid @enderror">
                                                     <span role="alert" class="invalid-feedback">
                                                         <strong>{{$errors->first('photo')}}</strong>
                                                     </span>
                                             </div>
-
                                             @isset($gallery)
                                                 @method('PUT')
-                                                <img src="{{asset('storage/'.$gallery->photo ) }}" class="img-fluid circle text-center" width="100" alt="" />
+                                                <img src="{{asset('storage/'.$gallery->photo ) }}" class="img-fluid mb-3" width="100" alt="" />
                                             @endisset
                                         </div>
-
                                         <div class="col-12">
                                             <div class="form-group text-center">
                                                 <button type="submit" class="btn btn-md ft-medium text-light rounded theme-bg">{{ isset($gallery) ?'Update ':'Add '}} Image</button>
                                             </div>
                                         </div>
-
                                     </div>
                                 </div>
                             </form>
                         </div>
                     </div>
                 </div>
-                @if($galleries->isNotEmpty())
-                    <div class="col-xl-12 col-lg-12 col-md-12">
-                        <div class="mb-4 tbl-lg rounded overflow-hidden">
-                            <div class="table-responsive bg-white">
-                                <table class="table">
-                                    <thead class="thead-dark">
-                                    <tr>
-                                        <th scope="col">S/N</th>
-                                        <th scope="col"></th>
-
-                                        <th scope="col">Short Description</th>
-                                        <th scope="col">Action</th>
-                                    </tr>
-                                    </thead>
-                                    <tbody>
-                                    @foreach($galleries as  $gallery)
+                @if(!isset($gallery))
+                    @if($galleries->isNotEmpty())
+                        <div class="col-xl-12 col-lg-12 col-md-12">
+                            <div class="mb-4 tbl-lg rounded overflow-hidden">
+                                <div class="table-responsive bg-white">
+                                    <table class="table">
+                                        <thead class="thead-dark">
                                         <tr>
-                                            <td><div class="dash-title"><h4 class="mb-0 ft-medium fs-sm">{{ $loop->iteration }}</h4></div></td>
-                                            <td><img src="{{asset('storage/'.$gallery->photo ) }}" class="img-fluid circle" width="70" alt="" /></td>
-                                            <td>{{ $gallery->description }}</td>
-                                            <td>
-                                                <div class="dash-action">
-                                                    <a href="javascript:void(0);" class="p-2 circle text-info bg-light-info d-inline-flex align-items-center justify-content-center mr-1"><i class="lni lni-eye"></i></a>
-                                                    <a href="" class="p-2 circle text-success bg-light-success d-inline-flex align-items-center justify-content-center"><i class="lni lni-pencil"></i></a>
-                                                    <a href="javascript:void(0);" class="p-2 circle text-danger bg-light-danger d-inline-flex align-items-center justify-content-center ml-1"><i class="lni lni-trash-can"></i></a>
-
-                                                    <a href="{{ route('testimonies.edit', $gallery->id)}}" class="p-2 circle text-success bg-light-success d-inline-flex align-items-center justify-content-center my-2"><i class="lni lni-pencil"></i></a>
-                                                    <button type="button" onclick="handleDelete({{ $gallery->id }})" class="p-2 circle text-danger bg-light-danger d-inline-flex align-items-center justify-content-center ml-1"><i class="lni lni-trash-can"></i></button>
-
-
-                                                </div>
-                                            </td>
+                                            <th scope="col">S/N</th>
+                                            <th scope="col"></th>
+                                            <th scope="col">Short Description</th>
+                                            <th scope="col">Action</th>
                                         </tr>
-                                    @endforeach
-
-                                    </tbody>
-                                </table>
+                                        </thead>
+                                        <tbody>
+                                        @foreach($galleries as  $gallery)
+                                            <tr>
+                                                <td><div class="dash-title"><h4 class="mb-0 ft-medium fs-sm">{{ $loop->iteration }}</h4></div></td>
+                                                <td><img src="{{asset('storage/'.$gallery->photo ) }}" class="img-fluid " width="70" alt="" /></td>
+                                                <td>{{ $gallery->description }}</td>
+                                                <td>
+                                                    <div class="dash-action">
+                                                        <a href="{{ route('galleries.edit', $gallery)}}" class="p-2 circle text-success bg-light-success d-inline-flex align-items-center justify-content-center my-2"><i class="lni lni-pencil"></i></a>
+                                                        <a href="javascript:void(0);" onclick="handleDelete({{ $gallery->id }})" class="p-2 circle text-danger bg-light-danger d-inline-flex align-items-center justify-content-center ml-1"><i class="lni lni-trash-can"></i></a>
+                                                    </div>
+                                                </td>
+                                            </tr>
+                                        @endforeach
+                                        </tbody>
+                                    </table>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                @else
-                    <div class="col-xl-12 col-lg-12 col-md-12">
-                        <div class="_dashboard_content bg-white rounded mb-4">
-                            <div class="_dashboard_content_body py-3 px-3">
-                                <div class="container">
-                                    <div class="row justify-content-center align-items-center">
-                                        <div class="text-center"> <i class="fa fa-4x fa-images" aria-hidden="true"></i>
+                    @else
+                        <div class="col-xl-12 col-lg-12 col-md-12">
+                            <div class="_dashboard_content bg-white rounded mb-4">
+                                <div class="_dashboard_content_body py-3 px-3">
+                                    <div class="container">
+                                        <div class="row justify-content-center align-items-center">
+                                            <div class="text-center"> <i class="fa fa-4x fa-images" aria-hidden="true"></i>
 
-                                            <h4>You do not have any images in the gallery.</h4>
-                                            <p>When you upload an image, it will appear here.</p>
+                                                <h4>You do not have any images in the gallery.</h4>
+                                                <p>When you upload an image, it will appear here.</p>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
+                    @endif
                 @endif
             </div>
         </div>
