f539db022c7f3e7fd2ddd65ec66a4376e7c73950
Tochukwu Ugwu <ugwukelvintochukwu@gmail.com>
2023-09-05 18:44:04 +0100

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -376,116 +376,119 @@
     <!-- ======================= Job List ======================== -->
 
     <section class="bg-cover" style="background:#ff0000 url({{ asset('assets/img/curve.svg') }})no-repeat">
-        <div class="container">
+        @if(false)
+            <div class="container">
 
-            <div class="row justify-content-center">
-                <div class="col-xl-5 col-lg-7 col-md-9 col-sm-12">
-                    <div class="sec_title position-relative text-center mb-5">
-                        <h6 class="text-light mb-0">Current Openings</h6>
-                        <h2 class="ft-bold text-light">We Have Worked with 10,000+ Trusted Companies</h2>
-                    </div>
-                </div>
-            </div>
-
-            <div class="row justify-content-center">
-                <div class="col-xl-10 col-lg-11 col-md-12 col-sm-12">
-                    <div class="row justify-content-center">
-                        <div class="col-xl-3 col-lg-4 col-md-4 col-sm-6 col-12">
-                            <div class="cats-wrap text-left">
-                                <a href="job-search-v1.html"
-                                   class="cats-box rounded bg-white d-flex align-items-center px-2 py-3">
-                                    <div class="text-center"><img src="assets/img/c-9.png" class="img-fluid" width="55"
-                                                                  alt=""></div>
-                                    <div class="cats-box-caption px-2">
-                                        <h4 class="fs-md mb-0 ft-medium">Web Designing</h4>
-                                        <span class="text-muted">302 Jobs</span>
-                                    </div>
-                                </a>
-                            </div>
-                        </div>
-                        <div class="col-xl-3 col-lg-4 col-md-4 col-sm-6 col-12">
-                            <div class="cats-wrap text-left">
-                                <a href="job-search-v1.html"
-                                   class="cats-box rounded bg-white d-flex align-items-center px-2 py-3">
-                                    <div class="text-center"><img src="assets/img/c-4.png" class="img-fluid" width="55"
-                                                                  alt=""></div>
-                                    <div class="cats-box-caption px-2">
-                                        <h4 class="fs-md mb-0 ft-medium">Web Designing</h4>
-                                        <span class="text-muted">302 Jobs</span>
-                                    </div>
-                                </a>
-                            </div>
-                        </div>
-                        <div class="col-xl-3 col-lg-4 col-md-4 col-sm-6 col-12">
-                            <div class="cats-wrap text-left">
-                                <a href="job-search-v1.html"
-                                   class="cats-box rounded bg-white d-flex align-items-center px-2 py-3">
-                                    <div class="text-center"><img src="assets/img/c-2.png" class="img-fluid" width="55"
-                                                                  alt=""></div>
-                                    <div class="cats-box-caption px-2">
-                                        <h4 class="fs-md mb-0 ft-medium">Web Designing</h4>
-                                        <span class="text-muted">302 Jobs</span>
-                                    </div>
-                                </a>
-                            </div>
-                        </div>
-                        <div class="col-xl-3 col-lg-4 col-md-4 col-sm-6 col-12">
-                            <div class="cats-wrap text-left">
-                                <a href="job-search-v1.html"
-                                   class="cats-box rounded bg-white d-flex align-items-center px-2 py-3">
-                                    <div class="text-center"><img src="assets/img/c-5.png" class="img-fluid" width="55"
-                                                                  alt=""></div>
-                                    <div class="cats-box-caption px-2">
-                                        <h4 class="fs-md mb-0 ft-medium">Web Designing</h4>
-                                        <span class="text-muted">302 Jobs</span>
-                                    </div>
-                                </a>
-                            </div>
-                        </div>
-                        <div class="col-xl-3 col-lg-4 col-md-4 col-sm-6 col-12">
-                            <div class="cats-wrap text-left">
-                                <a href="job-search-v1.html"
-                                   class="cats-box rounded bg-white d-flex align-items-center px-2 py-3">
-                                    <div class="text-center"><img src="assets/img/c-10.png" class="img-fluid" width="55"
-                                                                  alt=""></div>
-                                    <div class="cats-box-caption px-2">
-                                        <h4 class="fs-md mb-0 ft-medium">Web Designing</h4>
-                                        <span class="text-muted">302 Jobs</span>
-                                    </div>
-                                </a>
-                            </div>
-                        </div>
-                        <div class="col-xl-3 col-lg-4 col-md-4 col-sm-6 col-12">
-                            <div class="cats-wrap text-left">
-                                <a href="job-search-v1.html"
-                                   class="cats-box rounded bg-white d-flex align-items-center px-2 py-3">
-                                    <div class="text-center"><img src="assets/img/c-12.png" class="img-fluid" width="55"
-                                                                  alt=""></div>
-                                    <div class="cats-box-caption px-2">
-                                        <h4 class="fs-md mb-0 ft-medium">Web Designing</h4>
-                                        <span class="text-muted">302 Jobs</span>
-                                    </div>
-                                </a>
-                            </div>
-                        </div>
-                        <div class="col-xl-3 col-lg-4 col-md-4 col-sm-6 col-12">
-                            <div class="cats-wrap text-left">
-                                <a href="job-search-v1.html"
-                                   class="cats-box rounded bg-white d-flex align-items-center px-2 py-3">
-                                    <div class="text-center"><img src="assets/img/c-17.png" class="img-fluid" width="55"
-                                                                  alt=""></div>
-                                    <div class="cats-box-caption px-2">
-                                        <h4 class="fs-md mb-0 ft-medium">Web Designing</h4>
-                                        <span class="text-muted">302 Jobs</span>
-                                    </div>
-                                </a>
-                            </div>
+                <div class="row justify-content-center">
+                    <div class="col-xl-5 col-lg-7 col-md-9 col-sm-12">
+                        <div class="sec_title position-relative text-center mb-5">
+                            <h6 class="text-light mb-0">Current Openings</h6>
+                            <h2 class="ft-bold text-light">We Have Worked with 10,000+ Trusted Companies</h2>
                         </div>
                     </div>
                 </div>
-            </div>
 
-        </div>
+                <div class="row justify-content-center">
+                    <div class="col-xl-10 col-lg-11 col-md-12 col-sm-12">
+                        <div class="row justify-content-center">
+                            <div class="col-xl-3 col-lg-4 col-md-4 col-sm-6 col-12">
+                                <div class="cats-wrap text-left">
+                                    <a href="job-search-v1.html"
+                                       class="cats-box rounded bg-white d-flex align-items-center px-2 py-3">
+                                        <div class="text-center"><img src="assets/img/c-9.png" class="img-fluid" width="55"
+                                                                      alt=""></div>
+                                        <div class="cats-box-caption px-2">
+                                            <h4 class="fs-md mb-0 ft-medium">Web Designing</h4>
+                                            <span class="text-muted">302 Jobs</span>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="col-xl-3 col-lg-4 col-md-4 col-sm-6 col-12">
+                                <div class="cats-wrap text-left">
+                                    <a href="job-search-v1.html"
+                                       class="cats-box rounded bg-white d-flex align-items-center px-2 py-3">
+                                        <div class="text-center"><img src="assets/img/c-4.png" class="img-fluid" width="55"
+                                                                      alt=""></div>
+                                        <div class="cats-box-caption px-2">
+                                            <h4 class="fs-md mb-0 ft-medium">Web Designing</h4>
+                                            <span class="text-muted">302 Jobs</span>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="col-xl-3 col-lg-4 col-md-4 col-sm-6 col-12">
+                                <div class="cats-wrap text-left">
+                                    <a href="job-search-v1.html"
+                                       class="cats-box rounded bg-white d-flex align-items-center px-2 py-3">
+                                        <div class="text-center"><img src="assets/img/c-2.png" class="img-fluid" width="55"
+                                                                      alt=""></div>
+                                        <div class="cats-box-caption px-2">
+                                            <h4 class="fs-md mb-0 ft-medium">Web Designing</h4>
+                                            <span class="text-muted">302 Jobs</span>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="col-xl-3 col-lg-4 col-md-4 col-sm-6 col-12">
+                                <div class="cats-wrap text-left">
+                                    <a href="job-search-v1.html"
+                                       class="cats-box rounded bg-white d-flex align-items-center px-2 py-3">
+                                        <div class="text-center"><img src="assets/img/c-5.png" class="img-fluid" width="55"
+                                                                      alt=""></div>
+                                        <div class="cats-box-caption px-2">
+                                            <h4 class="fs-md mb-0 ft-medium">Web Designing</h4>
+                                            <span class="text-muted">302 Jobs</span>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="col-xl-3 col-lg-4 col-md-4 col-sm-6 col-12">
+                                <div class="cats-wrap text-left">
+                                    <a href="job-search-v1.html"
+                                       class="cats-box rounded bg-white d-flex align-items-center px-2 py-3">
+                                        <div class="text-center"><img src="assets/img/c-10.png" class="img-fluid" width="55"
+                                                                      alt=""></div>
+                                        <div class="cats-box-caption px-2">
+                                            <h4 class="fs-md mb-0 ft-medium">Web Designing</h4>
+                                            <span class="text-muted">302 Jobs</span>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="col-xl-3 col-lg-4 col-md-4 col-sm-6 col-12">
+                                <div class="cats-wrap text-left">
+                                    <a href="job-search-v1.html"
+                                       class="cats-box rounded bg-white d-flex align-items-center px-2 py-3">
+                                        <div class="text-center"><img src="assets/img/c-12.png" class="img-fluid" width="55"
+                                                                      alt=""></div>
+                                        <div class="cats-box-caption px-2">
+                                            <h4 class="fs-md mb-0 ft-medium">Web Designing</h4>
+                                            <span class="text-muted">302 Jobs</span>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="col-xl-3 col-lg-4 col-md-4 col-sm-6 col-12">
+                                <div class="cats-wrap text-left">
+                                    <a href="job-search-v1.html"
+                                       class="cats-box rounded bg-white d-flex align-items-center px-2 py-3">
+                                        <div class="text-center"><img src="assets/img/c-17.png" class="img-fluid" width="55"
+                                                                      alt=""></div>
+                                        <div class="cats-box-caption px-2">
+                                            <h4 class="fs-md mb-0 ft-medium">Web Designing</h4>
+                                            <span class="text-muted">302 Jobs</span>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        @endif
+
         <div class="ht-50"></div>
     </section>
 
